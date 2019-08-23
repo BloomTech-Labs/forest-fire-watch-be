25569a724ee55ce3f73f1cec7b5510be92e7788e
jackcornellober <jackcornellober@gmail.com>
2019-08-23 11:01:39 -0700

    made some seed users and locations

--- database/seeds/01-users.js
+++ database/seeds/01-users.js
@@ -1,7 +1,9 @@
 
 exports.seed = function(knex, Promise) {
   return knex('users').insert([   
-    { username: 'John A', address: "2920 Zoo Dr, San Diego, CA 92101" },
-    { username: 'Wendy Q', address: "Government Dr, St. Louis, MO 63110" }
+    { username: 'jessica', password: "$2a$10$CA/TRbXwKL1WdIQui3JInu8UckMV7oJqzN6.YXzZB29JFnTYm1SXi" },
+    { username: 'danielle', password: "$2a$10$SFhgvqb8S2Kwp493NBSuwO42etTo16VHxvWgr651MvhwWSFaw3f3O" },
+    { username: 'antonio', password: "$2a$10$SkexJCxGwlZJBoDrAl14uOFwevjjwKQt7C88z7lE/bD8PzpFmzt9a" },
+    { username: 'galileo', password: "$2a$10$O758JcAgqvujbo2DcfoY7u9U5lTHT9ylbg9x128JxGnTAH2KwB.uq" },
   ]);
 };
