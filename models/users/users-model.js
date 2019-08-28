af296f4bd08f6faab7bca18a7e06036a4ee75c01
jackcornellober <jackcornellober@gmail.com>
2019-08-28 01:06:59 -0700

    made another postgre grammar fix

--- models/users/users-model.js
+++ models/users/users-model.js
@@ -18,7 +18,7 @@ function findBy(filter) {
 }
 
 async function add(user) {
-  const [id] = await db('users').insert(user, "id");
+  const [id] = await db('users').insert(user, 'id');
 
   return findById(id);
 }
