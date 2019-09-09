38f093b64be80640ade9725b36431d11d87abe8d
ken1286 <kindle1286@gmail.com>
2019-09-09 14:05:26 -0700

    hid twilio account info in env

--- sms/twilio.js
+++ sms/twilio.js
@@ -1,19 +1,19 @@
-const accountSid = "AC6ee31540c8d5c46bbf2978260f88d56d"; // Account SID from www.twilio.com/console
-const authToken = "3ebffba67a2d23484378e87ac3f819cb"; // Auth Token from www.twilio.com/console
+const accountSid = process.env.TWILIO_ID; // Account SID from www.twilio.com/console
+const authToken = process.env.TWILIO_AUTH; // Auth Token from www.twilio.com/console
 
 const twilio = require("twilio");
 const client = new twilio(accountSid, authToken);
 
 module.exports = {
-  messageMe
+  alertMessage
 };
 
-function messageMe() {
+function alertMessage() {
   client.messages
     .create({
       body: "Hello from Node",
-      to: "+15628330376", // Text this number
-      from: "+14243961692" // From a valid Twilio number
+      to: "+12345678910", // Text this number 1(234)567-8910
+      from: "+14243961692" // Ken's test account
     })
     .then(message => console.log(message.sid));
 }
