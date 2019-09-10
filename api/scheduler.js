1c50d900d5076afb91b77229dfe378e0f81ab792
Michael Mangus <mjmangus16@yahoo.com>
2019-09-10 16:14:03 -0400

    added items to user table

--- api/scheduler.js
+++ api/scheduler.js
@@ -6,14 +6,18 @@ const Users = require("../models/users/users-model");
 
 // Add new geocoding into the add location api call.
 
-cron.schedule("* * * * *", function() {
-  console.log("running a task every minute");
+// cron.schedule("* * * * *", function() {
+//   console.log("running a task every minute");
 
-  // Step 1: loop through all users
-  // Step 2: loop through each users locations
-  // Step 3: send each location through the DS server
-  // Step 4: determine if location has fire data True / False
-  // Step 5: send alert via push notification / sms message if alert is true
-});
+// Step 1: Get all the fires in the USA.
+//  - Store in an array.
+
+// Step 2: loop through locations
+// Step 3: compare each location to the array of fire data
+// Step 4: determine if location has fire data True / False
+// Step 5: if true push the location name and user id into a separate array
+//    - loop through the array to get each locations user information.
+// Step 6: send alert via push notification / sms message if alert is true
+// });
 
 module.exports = cron;
--- database/migrations/20190729132209_create_users_table.js
+++ database/migrations/20190729132209_create_users_table.js
@@ -1,15 +1,18 @@
 exports.up = function(knex) {
-    return knex.schema.createTable('users', users => {
-      users.increments();
-  
-      users
-        .string('username', 128)
-        .notNullable()
-        .unique();
-      users.string('password', 128).notNullable();
-    });
-  };
-  
-  exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('users');
-  };
+  return knex.schema.createTable("users", users => {
+    users.increments();
+
+    users
+      .string("username", 128)
+      .notNullable()
+      .unique();
+    users.string("password", 128).notNullable();
+    users.integer("cell_number");
+    users.boolean("receive_sms").default(false);
+    users.boolean("receive_push").default(false);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("users");
+};
