448e683e1edb9dc36b11cc3ee0a823e7a0d96a21
Dora Belme <contact.dorabelme@gmail.com>
2019-11-07 21:53:26 -0500

    deleting subscriptions from array

--- models/push-notifications/notifications-model.js
+++ models/push-notifications/notifications-model.js
@@ -1,42 +1,49 @@
 const db = require('../../database/dbConfig.js');
 
-module.exports={
+module.exports = {
     add,
     find,
     findBy,
     findById,
     remove,
-    update
+    update,
+    removeWebNotificationsForUser
 }
 
-function find(){
-    return db('notifications').select('id','subscription');
+function find() {
+    return db('notifications').select('id', 'subscription');
 }
 
-function findBy(filter){
+function findBy(filter) {
     return db('notifications').where(filter);
 }
 
-async function add(subscription){
-    const [id]=await db('notifications').insert(subscription,'id');
+async function add(subscription) {
+    const [id] = await db('notifications').insert(subscription, 'id');
 
     return findById(id);
 }
 
-function findById(id){
+function findById(id) {
     return db('notifications')
-        .where({id})
+        .where({ id })
         .first();
 }
 
-function remove(id){
+function remove(id) {
     return db('notifications')
-        .where({id})
+        .where({ id })
         .del();
 }
 
-function update(id,changes){
+function removeWebNotificationsForUser(user_id) {
     return db('notifications')
-        .where({id})
-        .update(changes,'*');
+        .where({ user_id: user_id, type: 'web' })
+        .del();
+}
+
+function update(id, changes) {
+    return db('notifications')
+        .where({ id })
+        .update(changes, '*');
 }
--- push/helper.js
+++ push/helper.js
@@ -34,7 +34,9 @@ const push = async (id, configs) => {
         let subData;
         if (subscription.type == "web")
             subData = JSON.parse(subscription.subscription);
-        else subData = subscription.subscription;
+        else
+            subData = subscription.subscription;
+
         //if web and first sent to web
         if (subscription.type == "web" && !web) {
             try {
